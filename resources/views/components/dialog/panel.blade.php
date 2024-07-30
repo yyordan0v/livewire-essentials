5aec973ffbf2b3691731dde4563cab0ef6353ff5
Yordan Yordanov <yoyordanov94@gmail.com>
2024-07-30 16:20:17 +0300

    add drag to close on mobile for dialog

--- resources/views/components/dialog/panel.blade.php
+++ resources/views/components/dialog/panel.blade.php
@@ -12,11 +12,25 @@
         <div class="relative min-h-screen flex items-end sm:items-center justify-center p-0 sm:p-4">
             <div
                 x-dialog:panel
-                x-transition.in x-transition.out.opacity
+                x-transition.in
                 class="relative max-w-xl w-full bg-white rounded-t-xl sm:rounded-b-xl shadow-lg overflow-y-auto"
             >
+                <!-- Mobile: Top "grab" handle -->
+                <div
+                    x-data="{ startY: 0, currentY: 0, moving: false, get distance() { return this.moving ? Math.max(0, this.currentY - this.startY): 0 } }"
+                    x-on:touchstart="moving = true; startY = currentY = $event.touches[0].clientY"
+                    x-on:touchmove="currentY = $event.touches[0].clientY"
+                    x-on:touchend="if (distance > 100) $dialog.close(); moving = false;"
+                    x-effect="$el.parentElement.style.transform = 'translateY('+distance+'px)'"
+                    class="sm:hidden absolute top-[-10px] left-0 right-0 h-[50px]">
+                    <div class="flex justify-center pt-[16px]">
+                        <div class="bg-gray-400 rounded-full w-[10%] h-[5px]"></div>
+                    </div>
+                </div>
+
                 <!-- Close Button -->
-                <div class="absolute top-0 right-0 pt-4 pr-4">
+                <div class=" absolute top-0 right-0 pt-4 pr-4
+                ">
                     <button type="button" @click="$dialog.close()"
                             class="bg-gray-50 rounded-lg p-2 text-gray-600 focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-500 focus-visible:ring-offset-2">
                         <span class="sr-only">Close modal</span>
