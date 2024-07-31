ad9e3f76ca186bffd3cd24850fe54492a1f9a74e
Yordan Yordanov <yoyordanov94@gmail.com>
2024-07-31 19:59:10 +0300

    add indeterminate checkbox states

--- resources/views/components/order/index/check-all.blade.php
+++ resources/views/components/order/index/check-all.blade.php
@@ -1,6 +1,7 @@
 <div x-data="checkAll">
     <input
         @change="handleCheck"
+        x-ref="checkbox"
         type="checkbox" class="rounded border-gray-300 shadow">
 </div>
 
@@ -8,6 +9,37 @@
 <script>
     Alpine.data('checkAll', () => {
         return {
+            init() {
+                this.$wire.$watch('selectedOrderIds', () => {
+                    this.updateCheckAllState()
+                })
+
+                this.$wire.$watch('orderIdsOnPage', () => {
+                    this.updateCheckAllState()
+                })
+            },
+
+            updateCheckAllState() {
+                if (this.pageIsSelected()) {
+                    this.$refs.checkbox.checked = true
+                    this.$refs.checkbox.indeterminate = false
+                } else if (this.pageIsEmpty()) {
+                    this.$refs.checkbox.checked = false
+                    this.$refs.checkbox.indeterminate = false
+                } else {
+                    this.$refs.checkbox.checked = false
+                    this.$refs.checkbox.indeterminate = true
+                }
+            },
+
+            pageIsSelected() {
+                return this.$wire.orderIdsOnPage.every(id => this.$wire.selectedOrderIds.includes(id))
+            },
+
+            pageIsEmpty() {
+                return this.$wire.selectedOrderIds.length === 0
+            },
+
             handleCheck(e) {
                 e.target.checked ? this.selectAll() : this.deselectAll()
             },
