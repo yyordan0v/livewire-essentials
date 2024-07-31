6bff417e832c6451f2316531bd3b8fa6750a1a30
Yordan Yordanov <yoyordanov94@gmail.com>
2024-07-31 12:51:08 +0300

    clear livewire components

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -12,7 +12,7 @@
 </head>
 <body>
 <main class="flex justify-center items-start py-16 bg-slate-100 min-h-screen text-slate-800">
-    <livewire:edit-profile/>
+    Home
 </main>
 @livewireScripts
 </body>
--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,7 @@
 <?php
 
-use App\Livewire\EditProfile;
-use App\Livewire\ShowPosts;
-use App\Livewire\Signup;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/profile', EditProfile::class);
-Route::get('/signup', Signup::class);
-Route::get('/posts', ShowPosts::class);
+Route::get('/', function () {
+    return view('welcome');
+});
